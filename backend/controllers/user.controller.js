4426aa93d94f0712cc9402db60a4af81d9c21e99
Courtney Thang <courtneythang@icloud.com>
2025-04-04 11:25:40 -0400

    sidebar fix

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,32 +1,45 @@
-import Conversation from "../models/conversation.model.js";
+// import Conversation from "../models/conversation.model.js";
+import User from "../models/user.model.js";
 
 export const getUsersforSideBar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
 
-    const conversations = await Conversation.find({
-      participants: loggedInUserId,
-    }).populate("participants");
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
 
-    const otherUsers = new Map();
-
-    conversations?.forEach((convo) => {
-      convo?.participants?.forEach((user) => {
-        if (
-          user._id.toString().toLowerCase() !==
-          loggedInUserId.toString().toLowerCase()
-        ) {
-          otherUsers.set(user._id.toString(), user);
-        }
-      });
-    });
-
-    const sidebarUsers = Array.from(otherUsers.values());
-
-    res.status(200).json(sidebarUsers);
+    res.status(200).json(filteredUsers);
   } catch (error) {
-    console.error("Error in user controller:", error.message);
-
-    res.status(500).json({ errpr: `Internal server error: ${error.message}` });
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
+  // try {
+  //   const loggedInUserId = req.user._id;
+
+  //   const conversations = await Conversation.find({
+  //     participants: loggedInUserId,
+  //   }).populate("participants");
+
+  //   const otherUsers = new Map();
+
+  //   conversations?.forEach((convo) => {
+  //     convo?.participants?.forEach((user) => {
+  //       if (
+  //         user._id.toString().toLowerCase() !==
+  //         loggedInUserId.toString().toLowerCase()
+  //       ) {
+  //         otherUsers.set(user._id.toString(), user);
+  //       }
+  //     });
+  //   });
+
+  //   const sidebarUsers = Array.from(otherUsers.values());
+
+  //   res.status(200).json(sidebarUsers);
+  // } catch (error) {
+  //   console.error("Error in user controller:", error.message);
+
+  //   res.status(500).json({ errpr: `Internal server error: ${error.message}` });
+  // }
 };
